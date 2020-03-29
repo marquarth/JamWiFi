ed52343e87a616380a125dba37f8899f0a9849b1
Marquarth <oqueen917@gmail.com>
2020-03-29 22:30:51 +0800

    move to NSAlert

--- Wireless/HelperFunc.m
+++ Wireless/HelperFunc.m
@@ -4,5 +4,11 @@
 #import "HelperFunc.h"
 
 void runAlert(NSString*title, NSString*message) {
-	NSRunAlertPanel(title, message, @"OK", nil, nil);
+    // NSRunAlertPanel(title, @"%@", message, @"OK", nil, nil);
+    NSAlert *showAlert = [[NSAlert alloc] init];
+    [showAlert addButtonWithTitle:@"Coninue"];
+    [showAlert setMessageText:(message)];
+    [showAlert setAlertStyle:(NSAlertStyleInformational)];
+    
+    
 }
